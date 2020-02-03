b099c5bb0f202c6a557d251ca119e27c18a1a3d8
William Meighan <williamdmeighan@gmail.com>
2020-02-03 16:21:21 -0700

    update test

--- spec/features/reviews/user_can_create_review_spec.rb
+++ spec/features/reviews/user_can_create_review_spec.rb
@@ -1,20 +1,16 @@
 require 'rails_helper'
 
 describe "As a visitor" do
-
-  before :each do
-    @shelter_1 = Shelter.create(name: "Mike's Shelter", address: "1331 17th Street", city: "Denver", state: "CO", zip: "80202")
-  end
-
   describe "when I visit the a shelter show page" do
     it "I can click the link to fill out a form to add a new review" do
+      shelter_1 = Shelter.create(name: "Mike's Shelter", address: "1331 17th Street", city: "Denver", state: "CO", zip: "80202")
 
       title = "Good"
       rating = 5
       content = "I found my new best friend"
       image = "https://images-ra.adoptapet.com/images/Homepage-DogV2.png"
 
-      visit "/shelters/#{@shelter_1.id}"
+      visit "/shelters/#{shelter_1.id}"
 
       expect(page).to_not have_content(title)
       expect(page).to_not have_content(rating)
@@ -24,9 +20,7 @@ describe "As a visitor" do
       click_link 'Add Review'
 
       expect(current_path).to eq("/shelters/#{@shelter_1.id}/reviews/new")
-
-      expect(page).to have_content("Add a Review")
-
+      
       fill_in 'Title', with: title
       select rating, from: :rating
       fill_in 'Content', with: content
