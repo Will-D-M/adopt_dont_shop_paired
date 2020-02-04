05ba78c06b563f3e84dcc4e879ad246f0d360ae7
Elom Amouzou <elom1990@yahoo.com>
2020-02-03 21:18:44 -0700

    add validation test to review_spec.rb

---
+++ spec/models/review_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Review, type: :model do
+  describe 'relationships' do
+    it {should belong_to :shelter}
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :title
+      should validate_presence_of :rating
+      should validate_presence_of :content
+      should validate_presence_of :shelter
+      should validate_presence_of :picture}
+  end
+end
