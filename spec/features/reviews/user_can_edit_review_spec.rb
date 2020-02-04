c017dcf452117f9043896f2198b1e6338d18fa55
Elom Amouzou <elom1990@yahoo.com>
2020-02-04 08:29:45 -0700

    add to edit review test for flash message if field empty

--- spec/features/reviews/user_can_edit_review_spec.rb
+++ spec/features/reviews/user_can_edit_review_spec.rb
@@ -53,4 +53,59 @@ RSpec.describe 'edit shelter review' do
     expect(page).to have_css("img[src*='#{picture2}']")
   end
 
+  describe 'flash message for missing fields' do
+    scenario 'flashes message when user fails to enter all fields' do
+      visit "/shelters/#{@review1.id}/reviews/edit"
+
+      title2 = "Horrible"
+      rating2 = 1
+      content2 = "I kicked that dog out."
+      picture2 = "https://images-ra.adoptapet.com/images/Homepage-DogV2.png"
+
+      fill_in 'Title', with: ""
+      fill_in 'Rating', with: rating2
+      fill_in 'Content', with: content2
+      fill_in 'Picture', with: picture2
+
+      click_button 'Submit'
+
+      expect(current_path).to eq("/shelters/#{@review1.id}/reviews/edit")
+      expect(page).to have_content("A required field is missing; please fill out all fields!")
+
+      fill_in 'Title', with: title2
+      fill_in 'Rating', with: ""
+      fill_in 'Content', with: content2
+      fill_in 'Picture', with: picture2
+
+      click_button 'Submit'
+
+      expect(current_path).to eq("/shelters/#{@review1.id}/reviews/edit")
+      expect(page).to have_content("A required field is missing; please fill out all fields!")
+
+      fill_in 'Title', with: title2
+      fill_in 'Rating', with: rating2
+      fill_in 'Content', with: content2
+      fill_in 'Picture', with: ""
+
+      click_button 'Submit'
+
+
+      expect(current_path).to eq("/shelters/#{@review1.id}/reviews/edit")
+      expect(page).to have_content("A required field is missing; please fill out all fields!")
+
+      fill_in 'Title', with: title2
+      fill_in 'Rating', with: rating2
+      fill_in 'Content', with: content2
+      fill_in 'Picture', with: picture2
+
+      click_button 'Submit'
+
+      expect(current_path).to eq("/shelters/#{@shelter_1.id}")
+      expect(page).to have_content(title2)
+      expect(page).to have_content(rating2)
+      expect(page).to have_content(content2)
+      expect(page).to have_css("img[src*='#{picture2}']")
+    end
+  end
+
 end
