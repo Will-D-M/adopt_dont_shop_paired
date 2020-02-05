5a4725965c3b31e89f7f3851f8627d5dcb5763ef
Elom Amouzou <elom1990@yahoo.com>
2020-02-05 00:18:02 -0700

    add and revise update action after back and forth with model

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,11 @@
 class FavoritesController < ApplicationController
+  include ActionView::Helpers::TextHelper
 
   def update
     pet = Pet.find(params[:pet_id])
+    favorites.add_pet(pet.id)
+    session[:favorites] = favorites.pets_favorite
+    quantity = favorites.count_of(pet.id)
     flash[:favorite] = "The pet has been added to your favorites list."
     redirect_to "/pets/#{pet.id}"
   end
