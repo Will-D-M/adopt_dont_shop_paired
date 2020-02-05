a5e1c04358eb6fbf279f2f3f6e29a359693fa917
Elom Amouzou <elom1990@yahoo.com>
2020-02-05 10:28:01 -0700

    add index action (needs to be revised, toom many instance variables)

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,13 @@
 class FavoritesController < ApplicationController
   include ActionView::Helpers::TextHelper
 
+  def index
+    @pets = Pet.all
+    @fav_pets = favorites.pets_favorite.keys.map do |petid|
+      Pet.find(petid)
+    end
+  end
+
   def update
     pet = Pet.find(params[:pet_id])
     favorites.add_pet(pet.id)
