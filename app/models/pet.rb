f43741e96a07d5430741dae1cab2653d3b7cc3cb
Elom Amouzou <elom1990@yahoo.com>
2020-02-06 16:58:21 -0700

    add class method to pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,4 +2,11 @@ class Pet < ApplicationRecord
   belongs_to :shelter
   has_many :pet_applications, dependent: :destroy
   has_many :applications, through: :pet_applications
+
+  def self.favpets(favorites)
+    favorites.pets_favorite.keys.map do |petid|
+      Pet.find(petid)
+    end
+  end
+
 end
