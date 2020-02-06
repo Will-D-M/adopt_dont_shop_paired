98e6337f9f76b2fbe559ec148d24cf3fc77f1090
William Meighan <williamdmeighan@gmail.com>
2020-02-05 19:18:57 -0700

    correct spelling error

--- config/routes.rb
+++ config/routes.rb
@@ -28,5 +28,5 @@ Rails.application.routes.draw do
   get '/favorites', to: 'favorites#index'
   patch '/favorites/:pet_id', to: 'favorites#update'
   delete '/favorites/:pet_id', to: 'favorites#destroy'
-  delete '/favorites', to: 'favorite#destroy'
+  delete '/favorites', to: 'favorites#destroy'
 end
