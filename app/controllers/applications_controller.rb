f71204ddd6f7ddc457c2034d10327a810f3b981d
Elom Amouzou <elom1990@yahoo.com>
2020-02-06 10:03:52 -0700

    add to create action in apps controller, error about remove method

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -5,6 +5,22 @@ class ApplicationsController < ApplicationController
 
   def create
     application = Application.new(application_params)
+
+    if application.save == true
+      pets = Pet.find(params[:pet_ids])
+
+      pets.each {|pet| PetApplication.create(pet_id: pet.id, application_id: application.id)}
+      pets.each {|pet| favorites.remove_pet(pet.id)}
+
+      session[:favorites] = favorites.pets_favorite
+      flash[:submitted] = "Your application for the selected pets went through."
+      redirect_to '/favorites'
+
+    elsif application.save == false
+
+      flash[:missingfields] = "You must complete all fields in order for your application to be considered."
+      redirect_to '/applications/new'
+    end
   end
 
   private
