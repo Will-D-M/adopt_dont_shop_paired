387cc4e919f27a69d03dacc7c16edae6b2b678dd
Elom Amouzou <elom1990@yahoo.com>
2020-02-08 14:28:25 -0700

    added database diagram to readme, and played with active record

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,4 +4,15 @@ class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
 
+  def get_pet_name
+    #return pet name(s) for a given application
+    #match application id to pet_application's application id
+    #from that pet_application id, match pet_app's pet_id to pet table's id
+    # from that pet row, grab pet name
+    petids = PetApplication.where(application_id: self.id).pluck(:pet_id)
+    Pet.find(petids).pluck(:name)
+    # We start with the application id
+
+    #return pet(s) name for pets submitted applications for
+  end
 end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -14,4 +14,34 @@ RSpec.describe Application, type: :model do
     it {should have_many :pet_applications}
     it {should have_many(:pets).through(:pet_applications)}
   end
+
+  it '#get_pet_name' do
+    mikes_shelter = Shelter.create(name: "Mike's Shelter",
+      address: "1331 17th Street", city: "Denver", state: "CO", zip: "80202")
+
+    megs_shelter = Shelter.create(name: "Meg's Shelter", address: "150 Main Street",
+    city: "Hershey", state: "CO", zip: "17033")
+
+    athena = Pet.create(image: "https://images-ra.adoptapet.com/images/Homepage-DogV2.png",
+    name: "Athena",
+    # description: "butthead",
+    approximate_age: 1,
+    sex: "female",
+    # status: "adoptable",
+    shelter_name: "Mike's Shelter",
+    shelter_id: mikes_shelter.id)
+
+    odell = Pet.create(image: "https://imgix.bustle.com/uploads/getty/2019/11/18/6296727a-d38c-40b4-8ffe-dbec5cd1b289-getty-954967324.jpg?w=1020&h=576&fit=crop&crop=faces&auto=format&q=70",
+    name: "Odell",
+    # description: "good dog",
+    approximate_age: 4,
+    sex: "male",
+    # status: "adoptable",
+    shelter_name: "Meg's Shelter",
+    shelter_id: megs_shelter.id)
+    app1 = Application.create(name: "Hello", address: "123 E. Tray", city: "Denver", state: "CO", zip: "80034", phone_number: "7204678498", description: "i like pets")
+    petapp1 = PetApplication.create(application_id: app1.id, pet_id: odell.id)
+
+    expect(app1.get_pet_name).to eq(["Odell"])
+  end
 end
