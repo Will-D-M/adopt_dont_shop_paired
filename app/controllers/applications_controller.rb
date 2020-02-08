20dac448784cb74faeb512bbd3d4a7ad4efa0a67
Elom Amouzou <elom1990@yahoo.com>
2020-02-07 17:55:23 -0700

    add show action and instance variable

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -23,6 +23,10 @@ class ApplicationsController < ApplicationController
     end
   end
 
+  def show
+    @application = Application.find(params[:application_id])
+  end
+
   private
 
   def application_params
