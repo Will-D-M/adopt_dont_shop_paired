2358a5c7b5f0032d0014c4adcc61b6f1c95ebd78
William Meighan <williamdmeighan@gmail.com>
2020-02-10 11:09:25 -0700

    Need to validate presence of all shelter attributes

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,6 +2,12 @@ class Shelter < ApplicationRecord
   has_many :pets, dependent: :destroy
   has_many :reviews, dependent: :destroy
 
+  validates_presence_of :name
+  validates_presence_of :address
+  validates_presence_of :city
+  validates_presence_of :state
+  validates_presence_of :zip
+
   def pets_pending?
     pets.any? do |pet|
       pet.adoption_status == "pending"
