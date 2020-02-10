1c0c381f8dd14345e163aebb5f0408b12ba4e738
Elom Amouzou <elom1990@yahoo.com>
2020-02-09 17:32:37 -0700

    add get_pets instance method using activerecord

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -5,4 +5,9 @@ class PetApplication < ApplicationRecord
   def self.get_pets
     self.all.map {|app| Pet.find(app.pet_id)}
   end
+
+  def get_pets
+    petids = PetApplication.where(application_id: self.application_id).pluck(:pet_id)
+    Pet.find(petids)
+  end
 end
