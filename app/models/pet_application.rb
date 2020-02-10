a2b72df6b6d38104b10d33b9c252dfe3988688c7
Elom Amouzou <elom1990@yahoo.com>
2020-02-09 18:09:01 -0700

    remove get_pets class method from pet_application model; all tests pass

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -2,10 +2,6 @@ class PetApplication < ApplicationRecord
   belongs_to :pet
   belongs_to :application
 
-  def self.get_pets
-    self.all.map {|app| Pet.find(app.pet_id)}
-  end
-
   def get_pets
     petids = PetApplication.where(application_id: self.application_id).pluck(:pet_id)
     Pet.find(petids)
