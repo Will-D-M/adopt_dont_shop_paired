41e557990501c97c918b15c9ce2da0f913727375
William Meighan <williamdmeighan@gmail.com>
2020-02-09 18:42:02 -0700

    create test for pets_pending? helper method

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -8,4 +8,27 @@ RSpec.describe Shelter do
   describe 'relationships' do
     it { should have_many :reviews}
   end
+
+  describe '#pets_pending?' do
+    it 'returns true if any pets in a shelter are pending adoption' do
+      @shelter1 = Shelter.create(name: "Bloke",
+      address: "123456 E. Koko St.",
+      city: "Aville",
+      state: "CO",
+      zip: "83504")
+
+      pet1_path = "https://imgix.bustle.com/uploads/getty/2019/11/18/6296727a-d38c-40b4-8ffe-dbec5cd1b289-getty-954967324.jpg?w=1020&h=576&fit=crop&crop=faces&auto=format&q=70"
+
+      @pet1 = Pet.create(image: pet1_path,
+      name: "Patra",
+      approximate_age: 2,
+      sex: "free",
+      shelter_id: @shelter1.id,
+      shelter_name: @shelter1.name,
+      description: "cuddly",
+      adoption_status: "adoptable")
+
+      expect(@shelter1.pets_pending?).to eq(false)
+    end
+  end
 end
