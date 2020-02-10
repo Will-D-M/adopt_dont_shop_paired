e36e8cfce983c28c0590d93913e3b6f89c7f34a5
William Meighan <williamdmeighan@gmail.com>
2020-02-10 09:48:55 -0700

    refactor helper method

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,4 +1,11 @@
 class Shelter < ApplicationRecord
   has_many :pets, dependent: :destroy
   has_many :reviews, dependent: :destroy
+
+  def pets_pending?
+    pets.any? do |pet|
+      pet.adoption_status == "pending"
+    end
+  end
+
 end
