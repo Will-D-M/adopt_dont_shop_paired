fa762d78bb0ed791a911f96a8ec70eaf0619c674
William Meighan <williamdmeighan@gmail.com>
2020-02-10 11:21:13 -0700

    create model test for average rating

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -52,4 +52,28 @@ RSpec.describe Shelter do
       expect(@shelter1.pets_pending?).to eq(false)
     end
   end
+
+  describe '#average_rating' do
+    it "gives the average rating of the shelter" do
+      shelter1 = Shelter.create(name: "Bloke",
+      address: "123456 E. Koko St.",
+      city: "Aville",
+      state: "CO",
+      zip: "83504")
+
+      review_1 = Review.create!(title: "Great",
+                               rating: 4,
+                               content: "thanks",
+                               image: "https://imgix.bustle.com/uploads/getty/2019/11/18/6296727a-d38c-40b4-8ffe-dbec5cd1b289-getty-954967324.jpg?w=1020&h=576&fit=crop&crop=faces&auto=format&q=70",
+                               shelter: shelter1)
+
+      review_2 = Review.create!(title: "Amazing",
+                               rating: 5,
+                               content: "You the best",
+                               image: "https://imgix.bustle.com/uploads/getty/2019/11/18/6296727a-d38c-40b4-8ffe-dbec5cd1b289-getty-954967324.jpg?w=1020&h=576&fit=crop&crop=faces&auto=format&q=70",
+                               shelter: shelter1)
+
+      expect(shelter1.average_rating).to eq(4.5)
+    end
+  end
 end
