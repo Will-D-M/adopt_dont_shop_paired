75ab5af11e829a25238932d639272f0555b6a922
William Meighan <williamdmeighan@gmail.com>
2020-02-10 20:00:01 -0700

    Add link directing to pet show page from pet name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200210224143) do
+ActiveRecord::Schema.define(version: 20200210223302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 20200210224143) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "shelter_id"
-    t.string "adoption_status", default: "adoptable"
+    t.string "adoption_status"
     t.string "description", default: "Looking for a home"
     t.boolean "approved", default: false
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
