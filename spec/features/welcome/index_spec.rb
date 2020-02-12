a4845d165a9e98c99953bc35aa456638bf309cfe
William Meighan <williamdmeighan@gmail.com>
2020-02-11 17:15:11 -0700

    Add before each for welcome index

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 describe "As a visitor on the welcome index page" do
+  before :each do
+    visit "/"
+  end
+
   describe "there is a link to the pet index" do
     it "that takes me to the pet index" do
-      visit "/"
       click_link("All Pets")
       expect(current_path).to eq("/pets")
     end
@@ -11,7 +14,6 @@ describe "As a visitor on the welcome index page" do
 
   describe "there is a link to the shelters index" do
     it "that takes me to the shelters index" do
-      visit "/"
       click_link("All Shelters")
       expect(current_path).to eq("/shelters")
     end
@@ -19,14 +21,12 @@ describe "As a visitor on the welcome index page" do
 
   describe "there is a link to the favorites index" do
     it "that takes me to the favorites index" do
-      visit "/"
       click_link("Favorites: 0 pets")
       expect(current_path).to eq("/favorites")
     end
   end
 
   it "I see a welcome message, image, and footer" do
-    visit "/"
     expect(page).to have_content("Welcome to Adopt, Don't Shop!")
     expect(page).to have_content("Thank you for visiting our site!")
     expect(page).to have_content("You can access all of our shelters and/or all of our pets through the links above.")
