ebcac287f093864d97f3e1a22362335a9250fc13
William Meighan <williamdmeighan@gmail.com>
2020-02-12 07:06:19 -0700

    clean up

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -2,36 +2,22 @@ require 'rails_helper'
 
 describe 'As a visitor on the pets index page' do
   before :each do
-    @shelter1 = Shelter.create(name: "Bloke",
-    address: "123456 E. Koko St.",
-    city: "Aville",
-    state: "CO",
-    zip: "83504")
-    @shelter2 = Shelter.create(name: "Stevie",
-    address: "12765 E. Seesay St.",
-    city: "Aville",
-    state: "CO",
-    zip: "83571")
+    @shelter1 = Shelter.create( name: "Bloke",
+                                address: "123456 E. Koko St.",
+                                city: "Aville",
+                                state: "CO",
+                                zip: "83504")
 
     pet1_path = "https://imgix.bustle.com/uploads/getty/2019/11/18/6296727a-d38c-40b4-8ffe-dbec5cd1b289-getty-954967324.jpg?w=1020&h=576&fit=crop&crop=faces&auto=format&q=70"
-    pet2_path = "https://images-ra.adoptapet.com/images/Homepage-DogV2.png"
 
-    @pet1 = Pet.create(image: pet1_path,
-    name: "Patra",
-    approximate_age: 2,
-    sex: "free",
-    shelter_id: @shelter1.id,
-    shelter_name: @shelter1.name,
-    description: "cuddly",
-    adoption_status: "adoptable")
-    @pet2 = Pet.create(image: pet2_path,
-    name: "Shabba",
-    approximate_age: 5,
-    sex: "indigo",
-    shelter_id: @shelter2.id,
-    shelter_name: @shelter2.name,
-    description: "grumpy",
-    adoption_status: "adoptable")
+    @pet1 = Pet.create( image: pet1_path,
+                        name: "Patra",
+                        approximate_age: 2,
+                        sex: "free",
+                        shelter_id: @shelter1.id,
+                        shelter_name: @shelter1.name,
+                        description: "cuddly",
+                        adoption_status: "adoptable" )
 
     visit "/pets"
   end
